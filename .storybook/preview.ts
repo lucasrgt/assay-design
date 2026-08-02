aa108253a9bb39f978efef13c46347d93a10c63c
Lucas Tinoco <lucas.tinoco@hotmail.com>
2026-08-02 12:22:17 -0300

    pin contract workbench first in showcase

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,5 +4,6 @@ export default {
   parameters: {
     layout: 'fullscreen',
     controls: { disable: true },
+    options: { storySort: { order: ['Assay Design', ['Contract Workbench', 'Canonical Components']] } },
   },
 };
